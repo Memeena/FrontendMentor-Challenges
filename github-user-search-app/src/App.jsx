2ec45cb09adb48c71c6edd7d52931b54f3c88f15
Meenakshi <83496336+Memeena@users.noreply.github.com>
2024-01-29 18:06:34 +0530

    Header component

--- github-user-search-app/src/App.jsx
+++ github-user-search-app/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react"
 import Header from "./Header/Header"
+import Search from "./Search/Search";
 
 function App() {
   const [mode,setMode] = useState("Light");
 
+  function handleChangeMode(){
+    setMode((prev)=>prev.toLowerCase() === 'light' ? "dark" : "light");
+  }
+
   return (
     <div className="App">
-      <Header mode={mode}/>
+      <Header mode={mode} handleChangeMode={handleChangeMode}/>
+      <Search/>
     
     </div>
   )
--- github-user-search-app/src/Header/Header.jsx
+++ github-user-search-app/src/Header/Header.jsx
@@ -1,15 +1,17 @@
+import styles from "./Header.module.css"
 
+export default function Header({mode,handleChangeMode}){
+    console.log(mode);
 
-export default function Header({mode}){
-    console.log(mode)
-
-    const modeImg = `../../assets/${mode === "light" ? 'icon-sun.svg' : 'icon-moon.svg'}`
+    const modeName = mode.toLowerCase() === 'light' ? "dark" : "light";
+    const modeImg = `../../assets/${mode.toLowerCase() === "light" ? 'icon-moon.svg' : 'icon-sun.svg'}`;
+    
     return (
-        <header className="header">
-        <h1 className="heading">dev finder</h1>
-        <div className="mode">
-          <p className="mode-name">{mode}</p>
-          <img className="mode-img" src={modeImg} alt="mode"/>
+        <header className={`${styles.header} ${styles.flex}`}>
+        <h1 className={styles.heading}>dev finder</h1>
+        <div className={`${styles.mode} ${styles.flex}`} onClick={handleChangeMode}>
+          <p className={styles.modeName}>{modeName}</p>
+          <img className={styles.modeImg} src={modeImg} alt="mode"/>
         </div>
       </header>
     )
