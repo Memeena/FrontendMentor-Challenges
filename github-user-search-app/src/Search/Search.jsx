cdf36ad14e4c2449ebf50792a5ab85d9b559ae83
Meenakshi <83496336+Memeena@users.noreply.github.com>
2024-01-31 14:31:36 +0530

    Search field css changes

--- github-user-search-app/src/Search/Search.jsx
+++ github-user-search-app/src/Search/Search.jsx
@@ -4,17 +4,16 @@ export default function Search() {
   const searchIcon = "../../assets/icon-search.svg";
   return (
     <div className={styles.search}>
-
-    <form className={styles.searchForm}>
-    <img src={searchIcon} alt="search-icon" className={styles.searchIcon}/>
-      <label htmlFor="search"></label>
-      <input
-        id="search"
-        type="text"
-        placeholder="Search Github username..."
-        className={styles.input}></input>
-      <button className={styles.btn}>Search</button>
-    </form>
-        </div>
+      <form className={styles.searchForm}>
+        <img src={searchIcon} alt="search-icon" className={styles.searchIcon} />
+        {/* <label htmlFor="search"></label> */}
+        <input
+          id="search"
+          type="text"
+          placeholder="Search Github username..."
+          className={styles.input}></input>
+        <button className={styles.btn}>Search</button>
+      </form>
+    </div>
   );
 }
