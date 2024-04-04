28382639c73a92002404bac4908725f7a5cd310e
Meenakshi <83496336+Memeena@users.noreply.github.com>
2024-04-04 16:49:56 +0530

    final changes

--- frontend-quiz-app/src/components/Button/Button.js
+++ frontend-quiz-app/src/components/Button/Button.js
@@ -1,9 +1,7 @@
 import styles from "./Button.module.css";
 
 export default function Button({ children, onClick }) {
-  // function nextButton() {
-  //   console.log("i m clicked");
-  // }
+  
   return (
     <button onClick={onClick} className={styles.btn}>
       {children}
